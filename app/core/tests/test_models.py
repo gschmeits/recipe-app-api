d1a80b94f84545464e01a50f0e0232ad4c095a6c
Ger Schmeits <g.schmeits@squerist.nl>
2020-08-18 20:32:36 +0200

    Added list recipe feature.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -60,7 +60,7 @@ class ModelTest(TestCase):
         )
         self.assertEqual(str(ingredient), ingredient.name)
 
-    def test_recipy_str(self):
+    def test_recipe_str(self):
         """Test the recipe string representation"""
         recipe = models.Recipe.objects.create(
             user=sample_user(),
